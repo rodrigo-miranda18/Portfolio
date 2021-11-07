5b0907d84daac70ad9bfa88b7fc9359a3ecf083f
Rodrigo Miranda <rodrigoalvesmiranda@yahoo.com.br>
2021-11-07 19:41:27 -0300

    Add title and favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
+
 import Content from '../components/Content'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const Home = () => (
   <>
+    <Head>
+      <title>Rodrigo Miranda</title>
+    </Head>
+
     <Header />
     <Content />
     <Footer />
